085136efd4e07dbcafdbbb6f616a24597739e224
agreal2005 <139855646+agreal2005@users.noreply.github.com>
2024-06-09 11:59:54 +0530

    Created a struct for Eval Parameters

--- search.cpp
+++ search.cpp
@@ -6,15 +6,4 @@ EvalBar::EvalBar(string f)
     m.fetch_Moves(fen.board, fen.turn, fen.isEnPassant, fen.epSquare, fen.castle_options());
 }
 
-double EvalBar::complete_eval()
-{
-    /*
-        eval = sum of (weightage of an eval param*eval of the param)
-    */
-    double eval = 0.0;
-    //---------------To be completed------------------------------/
-    /*-----------------------------------------------------------*/
-    return eval;
-}
-
 // Make functions for complete evaluation and Game Search Tree here
