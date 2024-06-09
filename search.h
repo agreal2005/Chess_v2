015b5424a67ab515a2953951cbc3d7ba4e9f6a79
agreal2005 <139855646+agreal2005@users.noreply.github.com>
2024-06-09 11:45:40 +0530

    Search Tree begins

--- search.h
+++ search.h
@@ -6,12 +6,19 @@ class EvalBar
     private:
     Board_FEN fen; // Contains info about the board, turn, ep Square, Castling, etc.
     Moves m; // Contains info about pieces controlling squares, valid Moves, etc.
-    const int depth = 10;
+    const int depth = 5;
     
     public:
-    EvalBar(string fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+    EvalBar(string f = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+    string playOneMove(string move, vector<vector<char>> brd, bool t, bool wck, bool wcq, bool bck, bool bcq, bool isEnp, string epS, int hfc, int fms); // Return FEN
+    /*
+        Update all the boolean params
+        if valid Move is enPassant, then capture that pawn also
+        and see if after Valid move, there is en passant
+        Castling also has to be updated
+        Turn also has to be updated
+    */
+    pair<string, double> evalTree(string f, int d, bool isOpp = false); // First take the fen 'f' and send it to Dev
     double complete_eval(); // To be called when eval reaches its depth
-    //
-    // make a minimax function
-    //
+    
 };
--- test.cpp
+++ test.cpp
@@ -14,10 +14,11 @@ int main()
     v.display_board_FEN();
     Moves m;
     bool turn = v.return_turn();
+    pst.init_tables();
     m.fetch_Moves(v.return_board(), 0);
     cout << setw(60) << left << "Evaluate Checkmate: " << evaluate_checkmate(v.return_board(), m.return_oppControlSquares(), m.valid_Moves(), turn, fen) << endl;
     cout << setw(60) << left << "Evalute Material: " << evaluate_material(v.return_board()) << endl;
-    cout << setw(60) << left << "Pawn Structure: " << evaluate_pawn_structure(v.return_board(), m.return_controlSquares(), m.return_oppControlSquares(), turn, fen) << endl;
+    cout << setw(60) << left << "Pawn Structure: " << evaluate_pawn_structure(reverseBoard(v.return_board()), m.return_controlSquares(), m.return_oppControlSquares(), turn, fen) << endl;
     cout << setw(60) << left << "Evaluate Outposts: " << evaluate_outposts(v.return_board(), m.return_controlSquares(), m.return_oppControlSquares(), turn) << endl;
     cout << setw(60) << left << "Hanging Piece Penalty: " << hanging_piece_penalty(v.return_board(), m.return_controlSquares(), m.return_oppControlSquares(), turn) << endl;
     cout << setw(60) << left << "Weaker Attacked Penalty: " << weaker_attacked_penalty(v.return_board(), m.return_controlSquares(), m.return_oppControlSquares(), turn) << endl;
